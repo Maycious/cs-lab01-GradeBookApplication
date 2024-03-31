cee73b2741f05a2e194881137055d30a850a9ee3
Maciej Krawczyk <MacieQ12345@gmail.com>
2024-03-31 17:38:12 +0200

    RankedGradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+	public class RankedGradeBook : BaseGradeBook
+	{
+		public RankedGradeBook(string name) : base(name)
+		{
+			Type = GradeBookType.Ranked
+		}
+	}
+}
